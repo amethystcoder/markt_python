3de396136581d9003f7bb4fbe5767636b17f5d23
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-09-16 12:38:55 +0100

    Implement Cart model.

---
+++ app/models/cart_model.py
@@ -0,0 +1,19 @@
+from db import db
+
+
+class Cart(db.Model):
+    __tablename__ = "cart"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cart_id = db.Column(db.String(400), nullable=False)
+    buyer_id = db.Column(db.String(400), db.ForeignKey('buyer.unique_id'), nullable=False)
+    product_id = db.Column(db.String(400), db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    has_discount = db.Column(db.Boolean, nullable=False)
+    discount_price = db.Column(db.Float, nullable=False)
+    discount_percent = db.Column(db.Float, nullable=False)
+    order_status = db.Column(db.String(255), default='pending')
+
+    # Define relationships
+    buyer = db.relationship("Buyer", back_populates="cart_items")
+    product = db.relationship("Product", back_populates="carts")
