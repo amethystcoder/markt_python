a68362c2d9b0d0f770e773136ddb7057e157b74c
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-09-16 12:41:37 +0100

    Implement Payment model.

---
+++ app/models/payment_model.py
@@ -0,0 +1,20 @@
+from db import db
+
+
+class Payment(db.Model):
+    __tablename__ = "payment"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    payment_id = db.Column(db.String(400), nullable=False)
+    order_id = db.Column(db.String(400), db.ForeignKey('order.id'), nullable=False)
+    payment_method = db.Column(db.String(255), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    payment_status = db.Column(db.String(255), nullable=False)
+    payment_date = db.Column(db.DateTime, nullable=False)
+
+    # Define relationship
+    order = db.relationship("Order", back_populates="payments")
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
