9273a0741930ca577db9e9df694b2d587023e20e
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-09-17 17:00:30 +0100

    Configure API Documentation Settings.

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ import os
 
 class BaseConfig:
     # Add your base configurations here
+    API_TITLE = "Markt API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///data.db")
     # Other configurations
     # ...
