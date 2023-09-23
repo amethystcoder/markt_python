d9b6f574edcb5b3a79026c15ff8f64d6d6f0d784
amethystcoder <amethystguitaristj@gmail.com>
2023-09-23 23:35:15 +0100

    websocket setup and chat arrange/parse functions

--- app/models/chat_model.py
+++ app/models/chat_model.py
@@ -1,10 +1,10 @@
 from db import db
 
-
 class Chat(db.Model):
     """
     Controls all database crud operations related to user chats 
       """
+    
     __tablename__ = "chats"
 
     #An incremental id for the specified chat
@@ -14,8 +14,9 @@ class Chat(db.Model):
     #The message sent 
     message = db.Column(db.String(400), nullable=False)
     date_created = db.Column(db.TIMESTAMP, nullable=False)
-    sent_to = db.Column(db.String(400), nullable=False)
-    sent_from = db.Column(db.String(400), nullable=False)
+    recipent = db.Column(db.String(400), nullable=False)
+    #The user who sent the chat
+    sender = db.Column(db.String(400), nullable=False)
 
     def __init__(self,user_id):
         pass
@@ -23,9 +24,28 @@ class Chat(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+    
+    def arrange_chats(self, chats, user_id):
+        '''
+        arranges all unarranged chats of a particular user into packs of 
+        
+        '''
+        pass
 
-    def retrieve_chats_using_id(self,id):
-        db.session.select()
+    def retrieve_all_chats_using_user_id(self,user_id):
+        """
+        Gets all messages connected to a user(buyer or seller).
+        gets all messages, chats e.t.c from the database where the 
+        sender or recipent of the chat is the user with the `user_id`
+
+        Args:
+            user_id (String): _description_
+            The specified unique_id of the user(buyer/seller)
+        """
+        return db.session.query(Chat).get({'sender':user_id})
+    
+    def retrieve_chats_in_date_packets_using_user_id(self,user_id):
+        return db.session.query(Chat).get({'sent_from':user_id})
 
     def delete_from_db(self):
         db.session.delete(self)
--- app/routes/websocket.py
+++ app/routes/websocket.py
@@ -1,31 +1,37 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from json import decoder,encoder
+from flask_socketio import SocketIO, emit, send
+import json
 
 ChatWSapp = Flask(__name__)
-socketio = SocketIO(ChatWS)
+socketio = SocketIO(ChatWSapp)
 
 @ChatWSapp.route('/')
 def index():
     return 
 
+#clients = test_client.SocketIOTestClient.clients
 clients_cache = {}
 
 @socketio.on('connect')
-def test_connect():
-    socketio.emit('after connect', {'data', 'hello'})
+def on_connection_established():
+    socketio.emit('connect',{'message':'conected'})
     
 @socketio.on('message')
 def message_parse(message):
-    decoded_message = decoder.JSONDecoder.decode(message)
-    match decoded_message['type']:
-        case '':
-            pass
-        case '':
-            pass
-        case _:
+    decoded_message = json.loads(message)
+    if decoded_message is not None:
+        if decoded_message['type'] is not None:
+            match decoded_message['type']:
+                case 'register':
+                    decoded_message['register_id']
+                    pass
+                case 'message':
+                    pass
+                case _:
+                    pass
+        else:
             pass
 
 
 if __name__ == '__main__':
-    socketio.run(ChatWSapp)
+    socketio.run(ChatWSapp,port=3000)
