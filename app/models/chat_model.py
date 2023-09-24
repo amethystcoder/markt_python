1717d70c9fb43900f2ad64ebd1d0f33e7ce9f7a7
amethystcoder <amethystguitaristj@gmail.com>
2023-09-24 23:02:03 +0100

    more additions to the chat websockets and model

--- app/models/chat_model.py
+++ app/models/chat_model.py
@@ -1,8 +1,7 @@
 from db import db
 
 class Chat(db.Model):
-    """
-    Controls all database crud operations related to user chats 
+    """Controls all database crud operations related to user chats 
       """
     
     __tablename__ = "chats"
@@ -26,15 +25,71 @@ class Chat(db.Model):
         db.session.commit()
     
     def arrange_chats(self, chats, user_id):
+        '''arranges all unarranged chats of a particular user into bundles of 
+        messages
+        chats is (usually) a tuple containing the chat data gotten from the database
+        apparently, chat[5] is the recipent and chat[6] would be the sender. If you have 
+        any revisions or optimizations, please notify and make them.
+        user_id is the unique_id of the chat sender
         '''
-        arranges all unarranged chats of a particular user into packs of 
+        arranged_messages = []
         
-        '''
-        pass
+        if type(chats) == list:
+            for chat in chats:
+                #A flag that determines whether a message has been added to the arranged_messages
+                added = False
+                if type(chat) == tuple:
+                    ''' The message bundle is a packet of a particular user(buyer/seller) the 
+                            user with the user_id in the function arg has messaged or chatted with.
+                            The bundle contains details about that particular user, and an array of 
+                            the messages between the user and the user with the user_id in the function arg'''
+                    for arranged_message in arranged_messages:
+                        if arranged_message['user_id'] == chat[5] or arranged_message['user_id'] == chat[6]:
+                            arranged_message['messages'].append(
+                                {
+                                    "sent_to":chat[5],
+                                    "sent_from":chat[6],
+                                    "status":"",
+                                    "send_date_and_time":chat[4],
+                                    "message":chat[3]
+                                }
+                            )
+                            added = True
+                            break
+                    if not added:
+                        #TODO: We need to get the other data like `username`,`user_profile_image` and `user_type`
+                        #from the other classes/models. For now, empty strings would be assigned to them.
+                        
+                        new_message_bundle = {
+                            'user_id':'',
+                            'user_name':'',
+                            'user_profile_image':'',
+                            'user_type':'',
+                            'messages':[]
+                        }
+                        
+                        #setting the user_id of the dictionary chat bundle
+                        if user_id == chat[5]:
+                            new_message_bundle['user_id'] = chat[6]
+                        elif user_id == chat[6]:
+                            new_message_bundle['user_id'] = chat[5]
+                            
+                        new_message_bundle['messages'].append(
+                            {
+                                "sent_to":chat[5],
+                                "sent_from":chat[6],
+                                "status":"",
+                                "send_date_and_time":chat[4],
+                                "message":chat[3]
+                            }
+                        )
+                        arranged_messages.append(new_message_bundle)
+                        added = True
+                            
+        return arranged_messages
 
     def retrieve_all_chats_using_user_id(self,user_id):
-        """
-        Gets all messages connected to a user(buyer or seller).
+        """Gets all messages connected to a user(buyer or seller).
         gets all messages, chats e.t.c from the database where the 
         sender or recipent of the chat is the user with the `user_id`
 
@@ -42,7 +97,7 @@ class Chat(db.Model):
             user_id (String): _description_
             The specified unique_id of the user(buyer/seller)
         """
-        return db.session.query(Chat).get({'sender':user_id})
+        return self.arrange_chats(db.session.query(Chat).get({'sender':user_id}))
     
     def retrieve_chats_in_date_packets_using_user_id(self,user_id):
         return db.session.query(Chat).get({'sent_from':user_id})
--- app/routes/websocket.py
+++ app/routes/websocket.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, send
+from flask import Flask, render_template, request,session
+from flask_socketio import SocketIO, emit, join_room
 import json
+from ..models import chat_model
 
 ChatWSapp = Flask(__name__)
 socketio = SocketIO(ChatWSapp)
@@ -9,29 +10,45 @@ socketio = SocketIO(ChatWSapp)
 def index():
     return 
 
-#clients = test_client.SocketIOTestClient.clients
-clients_cache = {}
+clients_cache = []
 
-@socketio.on('connect')
+@socketio.on('open')
 def on_connection_established():
-    socketio.emit('connect',{'message':'conected'})
+    emit('connect',{'message':'conected'})
     
 @socketio.on('message')
 def message_parse(message):
+    '''
+     
+    '''
     decoded_message = json.loads(message)
     if decoded_message is not None:
         if decoded_message['type'] is not None:
             match decoded_message['type']:
                 case 'register':
-                    decoded_message['register_id']
-                    pass
+                    clients_cache.append({"client_id":decoded_message['register_id']})
+                    
+                    join_room(decoded_message['register_id'])
                 case 'message':
+                    emit('message',decoded_message,room=decoded_message['sent_to'])
+                    #TODO: store sent message to the database
+                    chat = chat_model.Chat()
+                    
                     pass
                 case _:
                     pass
         else:
             pass
 
+
+@socketio.on("error")
+def handle_error():
+    pass
+
+@socketio.on("close")
+def close_connection(user_id):
+    socketio.close_room(user_id)
+    pass
 
 if __name__ == '__main__':
     socketio.run(ChatWSapp,port=3000)
