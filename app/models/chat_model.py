26e87688479fd33e466032a75e1ede5191603453
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-10-07 09:43:28 +0100

    Update the Chat model in chat_model.py

--- app/models/chat_model.py
+++ app/models/chat_model.py
@@ -1,6 +1,5 @@
 from db import db
 from datetime import date
-from .group_chats import group_chats
 
 import hashlib
 import uuid
@@ -10,7 +9,8 @@ class Chat(db.Model):
     __tablename__ = "chats"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    message = db.Column(db.String(400), nullable=False)
+    message_type = db.Column(db.String(20), nullable=False)  # 'text', 'image', 'product'
+    message_content = db.Column(db.String(400))  # For text and image, for product, consider a relationship
     timestamp = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
     sender = db.Column(db.String(400), db.ForeignKey("users.id"), nullable=False)
     recipient = db.Column(db.String(400), db.ForeignKey("users.id"), nullable=False)
@@ -19,12 +19,11 @@ class Chat(db.Model):
     sender_user = db.relationship("User", foreign_keys=[sender], back_populates="sent_chats")
     recipient_user = db.relationship("User", foreign_keys=[recipient], back_populates="received_chats")
 
-    def __init__(self, unique_id, message, timestamp, recipient, sender):
-        self.unique_id = unique_id
-        self.message = message
-        self.timestamp = timestamp
-        self.recipient = recipient
+    def __init__(self, message_type, message_content, sender, recipient):
+        self.message_type = message_type
+        self.message_content = message_content
         self.sender = sender
+        self.recipient = recipient
 
     @staticmethod
     def generate_unique_id():
@@ -42,41 +41,3 @@ class Chat(db.Model):
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-
-    def retrieve_all_chats_between_users(self, user_id, date_start, date_end):
-        """
-        Retrieves all chats between the current user and another user within a specified date range.
-
-        Args:
-            user_id (str): The unique_id of the current user.
-            date_start (date): The start date for filtering chats.
-            date_end (date): The end date for filtering chats.
-
-        Returns:
-            list: List of dictionaries representing chat bundles.
-        """
-        return group_chats(
-            Chat.query.filter(
-                (Chat.sender == user_id) | (Chat.recipient == user_id),
-                (date_start <= Chat.timestamp <= date_end)
-            ).order_by(Chat.timestamp.asc()).all()
-        )
-
-    def retrieve_chats_between_users(self, user1_id, user2_id, date_start, date_end):
-        """
-        Retrieves chats between two users within a specified date range.
-
-        Args:
-            user1_id (str): The unique_id of the first user.
-            user2_id (str): The unique_id of the second user.
-            date_start (date): The start date for filtering chats.
-            date_end (date): The end date for filtering chats.
-
-        Returns:
-            list: List of dictionaries representing chat bundles.
-        """
-        return Chat.query.filter(
-            ((Chat.sender == user1_id) & (Chat.recipient == user2_id)) |
-            ((Chat.sender == user2_id) & (Chat.recipient == user1_id)),
-            (date_start <= Chat.timestamp <= date_end)
-        ).order_by(Chat.timestamp.asc()).all()
