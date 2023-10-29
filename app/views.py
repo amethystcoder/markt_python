b42442dfc5b0cafa1c927038048c5676030276a5
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-10-29 20:07:26 +0100

    updated views.py

--- app/views.py
+++ app/views.py
@@ -6,3 +6,39 @@ from db import db
 from app import socketio
 
 views = Blueprint('views', __name__, static_folder='static', template_folder='templates')
+
+# NOTE, we create a new chat list for any newly registered user (the register route)
+
+
+# Login decorator to ensure user is logged in before accessing certain routes
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if "user" not in session:
+            return redirect(url_for("views.login"))
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+# Register a new user and hash password
+@views.route("/register", methods=["GET", "POST"])
+def register():
+    """
+    Handles user registration and password hashing.
+
+    Returns:
+        Response: Flask response object.
+    """
+    pass
+
+
+@views.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    Handles user login and session creation.
+
+    Returns:
+        Response: Flask response object.
+    """
+    pass
