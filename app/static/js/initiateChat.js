8c8d2cf65cb0ffa2f56c45b571a5162f07f40d7e
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-10-30 16:43:12 +0100

    create/ initiate new chat script examples

---
+++ app/static/js/initiateChat.js
@@ -0,0 +1,16 @@
+// Link all html templates with the chat messsage/icon to this script
+
+function createChat(recipientId) {
+    $.ajax({
+        url: "/new-chat/?recipient_id=" + recipientId,
+        type: "GET",  // or "POST" depending on the route definition
+        success: function(response) {
+            console.log("Chat created successfully");
+            // Handle success response if needed
+        },
+        error: function(error) {
+            console.error("Error creating chat", error);
+            // Handle error response if needed
+        }
+    });
+}
