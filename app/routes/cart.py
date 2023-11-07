9cd9aa63eabe8f8bb16ce150ff8bb3690dd702ce
amethystcoder <amethystguitaristj@gmail.com>
2023-11-07 21:00:02 +0000

    added routes and functions for CRUD on orders

--- app/routes/cart.py
+++ app/routes/cart.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 from flask import abort
 from ..schemas import CartSchema
 from ..models.cart_model import Cart
+from ..models.order_model import Order
 from ..models.product_model import Product
 from ..models.imagename_store_model import ImageNameStore
 
@@ -47,6 +48,26 @@ class CartItem(MethodView):
       cart.delete_from_db()
     except Exception as e:
       abort(404,"cart item not found")
+      
+@cart_bp.route("/checkout/<buyer_id>")
+class CheckoutCart(MethodView):
+  @cart_bp.response(201, CartSchema)
+  def post(cartdata):
+    try:
+      buyer_cart_items = Cart.get_buyer_cart_items(buyer_id=cartdata["buyer_id"])
+      for cart_items in buyer_cart_items:
+        product = Product(cart_items.product_id)
+        if cart_items.has_discount is True and cart_items.discount_percent > 0:
+          #check if this is valid in the client side
+          product_total_price = cart_items.quantity * (product.price - ((cart_items.discount_percent / 100) * product.price))
+        else:
+          product_total_price = cart_items.quantity * product.price
+        #set the delivery address using the actual address of the buyer as default
+        order = Order(buyer_id=cart_items.buyer_id,product_id=product.product_id,seller_id=product.seller_id,
+                      total_price=product_total_price,quantity=cart_items.quantity,delivery_address="")
+        order.save_to_db()
+    except Exception as e:
+      abort(500,"Could not save")
         
     
 def parsecart(cartitem,image):
--- app/routes/order.py
+++ app/routes/order.py
@@ -3,6 +3,11 @@ from flask.views import MethodView
 from flask import abort
 from app.models.order_model import Order
 from app.models.user_model import User
+from app.models.seller_model import Seller
+from app.models.buyer_model import Buyer
+from app.models.product_model import Product
+
+from app.models.imagename_store_model import ImageNameStore
 
 from app.schemas import OrderSchema
 
@@ -10,23 +15,119 @@ order_bp = Blueprint("Orders", "orders", description="Endpoints and routes for e
 
 @order_bp.route("/new")
 class Order(MethodView):
-    @order_bp(201, OrderSchema)
-    def post(self,data):
-        try:
-           pass 
-        except Exception as e:
-            abort(500,"could not create")
+  @order_bp.response(201, OrderSchema)
+  def post(self,data):
+    try:
+      order = Order(buyer_id = data["buyer_id"],seller_id = data["seller_id"],product_id = data["product_id"],
+                    quantity = data["quantity"],total_price = data["total_price"],
+                    delivery_address = data["delivery_address"])
+      order.save_to_db()
+    except Exception as e:
+        abort(500,"could not create")
             
-@order_bp.route("/<seller_id>")
+@order_bp.route("/sellers/pending/<seller_id>")
 class PendingOrders(MethodView):
-    @order_bp(201, OrderSchema)
-    def get(self,data):
-        try:
-           pass 
-        except Exception as e:
-            abort(500,"could not create")
+  @order_bp.response(200, OrderSchema)
+  def get(self,seller_id):
+    try:
+        return [parse_unaccepted_orders(order=order,buyer=Buyer(order.buyer_id),product=Product(order.product_id),product_image=ImageNameStore.getproductthumbnail(order.product_id)) for order in Order.get_seller_pending_orders(seller_id)]
+    except Exception as e:
+        abort(500,"could not create")
             
+@order_bp.route("/sellers/accepted/<seller_id>")
+class AcceptedOrders(MethodView):
+  @order_bp.response(200, OrderSchema)
+  def get(self,seller_id):
+    try:
+        [parse_unaccepted_orders(order=order,buyer=Buyer(order.buyer_id),product=Product(order.product_id),product_image=ImageNameStore.getproductthumbnail(order.product_id)) for order in Order.get_seller_accepted_orders(seller_id)]
+    except Exception as e:
+      abort(500,"could not create")
+      
+@order_bp.route("/sellers/update/accept")
+class AcceptOrders():
+  @order_bp.response(200, OrderSchema)
+  #authentication would be needed
+  def patch(self,order_id):
+    try:
+      order = Order(order_id=order_id)
+      order.accept_order()
+    except Exception as e:
+      abort(404,"order not found")
+
+@order_bp.route("/sellers/update/decline")
+class DeclineOrders():
+  @order_bp.response(200, OrderSchema)
+  #authentication would be needed
+  def patch(self,order_id):
+    try:
+      order = Order(order_id=order_id)
+      order.decline_order()
+    except Exception as e:
+      abort(404,"order not found")
+
+@order_bp.route("/buyers/<buyer_id>")
+class BuyerOrders():
+  @order_bp.response(200, OrderSchema)
+  def get(self,buyer_id):
+    try:
+        [parse_buyer_orders(order=order,buyer=Buyer(order.buyer_id),product=Product(order.product_id),seller=Seller(order.seller_id),product_image=ImageNameStore.getproductthumbnail(order.product_id)) for order in Order.get_buyer_orders(buyer_id)]
+    except Exception as e:
+      abort(500,"could not create")
             
+def parse_unaccepted_orders(order,buyer,product,product_image):
+    return {
+        "order_id":order.order_id,
+        "seller_id":order.seller_id,
+        "product_quantity":order.quantity,
+        "order_date":order.order_date,
+        "product_name":product.name,
+        "total_price":order.total_price,
+        "product_id":order.product_id,
+        "product_image":product_image,
+        "buyer_id":order.buyer_id,
+        "buyer_name":buyer.username
+        }
+
+def parse_orders(order,seller,buyer,product,product_image):
+    return {
+        "order_id":order.order_id,
+        "seller_id":order.seller_id,
+        "product_quantity":order.quantity,
+        "order_date":order.order_date,
+        "product_name":product.name,
+        "total_price":order.total_price,
+        "product_id":order.product_id,
+        "product_image":product_image,
+        "buyer_id":order.buyer_id,
+        "buyer_name":buyer.username
+        }
+
+def parse_buyer_orders(order: Order,seller,buyer,product,product_image):
+    return {
+      "order_id":order.order_id,
+        "seller_id":order.seller_id,
+        "product_quantity":order.quantity,
+        "order_date":order.order_date,
+        "product_name":product.name,
+        "total_price":order.total_price,
+        "product_id":order.product_id,
+        "product_image":product_image,
+        "buyer_id":order.buyer_id,
+        "buyer_name":buyer.username,
+        "seller_shopname":seller.shop_name,
+        "order_status":order.order_status,
+        "delivery_name":"",
+        "delivery_id":"",
+        "received_by_delivery":False
+        }
+
+def parse_successful_orders(order,seller,buyer,product,product_image):
+    return {
+      "seller_id":order.seller_id,
+      "product_name":product.name,
+      "total_price":order.total_price,
+      "product_quantity":order.quantity
+        }
 """
 This is a list of functions i took from the client side that interact with the order backend from the
 php backend. I am just adding them here so i can be reminded of the endpoints i want to add.
