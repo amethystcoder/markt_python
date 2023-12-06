b34ab632a85c5be856c4b901fe1832ffa3d2fe4c
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-12-06 14:47:19 +0100

    refactored amethycoder's image_resizer_and_uploader.py >> utils

--- app/utilis/image_resizer_and_uploader.py
+++ app/utilis/image_resizer_and_uploader.py
@@ -3,16 +3,20 @@ import uuid
 from rembg import remove
 import os
 
+
+def generate_image_name():
+    return str(uuid.uuid5)
+
+
 class ImageSaver:
-    
-    #images can only be of type image with .jpg, .png and .webp extensions
-    allowed_extensions = ['jpg','png','webp']
-    
-    allowed_mimes = ['image/jpeg','image/png','image/webp']
-    
-    #5mb max file size
+    # images can only be of type image with .jpg, .png and .webp extensions
+    allowed_extensions = ['jpg', 'png', 'webp']
+
+    allowed_mimes = ['image/jpeg', 'image/png', 'image/webp']
+
+    # 5mb max file size
     max_file_size = 5 * 1024 * 1024
-    
+
     """
     This file contains functions for reducing image file sizes, storing the images in
     the upload folder or storage bucket, and also unlinking(deleting) image files from
@@ -20,8 +24,9 @@ class ImageSaver:
     from one format to another, rasterize and change the image structure and a whole lot of
     other stuff
     """
+
     @classmethod
-    def compressimage(self,image_location):
+    def compress_image(cls, image_location):
         """compress an image
         
         Keyword arguments:
@@ -30,18 +35,18 @@ class ImageSaver:
         """
         try:
             with Image.open(image_location) as image:
-                width,height = image.size
+                width, height = image.size
                 new_width = width * 0.3
                 new_height = height * 0.3
-                image.resize((new_width,new_height))
-                image_name = self.generateimagename()
-                image.save("app/uploads/"+image_name+".jpg","JPEG",optimize=True,quality=50)
+                image.resize((new_width, new_height))
+                image_name = generate_image_name()
+                image.save("app/uploads/" + image_name + ".jpg", "JPEG", optimize=True, quality=50)
                 return image_name
         except OSError:
             return False
-        
+
     @classmethod
-    def compress_image_and_remove_background(self,image_location):
+    def compress_image_and_remove_background(cls, image_location):
         """compress an image
         
         Keyword arguments:
@@ -50,38 +55,34 @@ class ImageSaver:
         """
         try:
             with Image.open(image_location) as image:
-                width,height = image.size
+                width, height = image.size
                 new_width = width * 0.3
                 new_height = height * 0.3
-                image.resize((new_width,new_height))
-                image_name = self.generateimagename()
+                image.resize((new_width, new_height))
+                image_name = generate_image_name()
                 image_with_background_removed = remove(image)
-                image_with_background_removed.save("app/uploads/"+image_name+".jpg","JPEG",optimize=True,quality=50)
+                image_with_background_removed.save("app/uploads/" + image_name + ".jpg", "JPEG", optimize=True,
+                                                   quality=50)
                 return image_name
         except OSError:
             return False
-        
 
     @classmethod
-    def is_valid_file(self,name):
-        return '.' in name and name.rsplit('.',1)[1].lower() in self.allowed_extensions
-    
-    @classmethod
-    def is_valid_mime(self,mime_to_check):
-        return mime_to_check in self.allowed_mimes
-    
-    @classmethod
-    def is_valid_file_size(self,filesize):
-        return filesize < self.max_file_size
-    
-    @classmethod
-    def generateimagename(self):
-        return str(uuid.uuid5())
+    def is_valid_file(cls, name):
+        return '.' in name and name.rsplit('.', 1)[1].lower() in cls.allowed_extensions
 
     @classmethod
-    def deleteimage(self,image_name):
+    def is_valid_mime(cls, mime_to_check):
+        return mime_to_check in cls.allowed_mimes
+
+    @classmethod
+    def is_valid_file_size(cls, file_size):
+        return file_size < cls.max_file_size
+
+    @staticmethod
+    def delete_image(self, image_name):
         try:
-            os.unlink("app/routes/"+image_name+".jpg")
+            os.unlink("app/routes/" + image_name + ".jpg")
             return True
         except OSError:
             return False
