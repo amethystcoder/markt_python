2aaaa63cab11d48364b2c3a330991162eecdb22f
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-12-29 14:42:48 +0100

    feat fixing bugs/errors from test: missing configs

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ class BaseConfig:
     OPENAPI_VERSION = "3.0.3"
     OPENAPI_URL_PREFIX = "/"
     OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///data.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = "filesystem"  # Flask-Login requires this for session management
     MAIL_SERVER = ''  # i.e smtp.gmail.com
     MAIL_PORT = 0  # i.e 8080
