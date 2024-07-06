f90e636b6f8ee56c1dabb57fb54759dc50692c62
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-07-06 10:24:41 +0100

    Improvements: redundant imports, error handling

--- app/models/favorites_model.py
+++ app/models/favorites_model.py
@@ -1,7 +1,4 @@
 from db import db
-
-
-import hashlib
 import uuid
 
 
@@ -9,57 +6,41 @@ class Favorite(db.Model):
     __tablename__ = "favorites"
 
     id = db.Column(db.Integer, primary_key=True)
-    # buyer_id = db.Column(db.String(400), db.ForeignKey('buyers.unique_id'), nullable=False)
-    favorite_item_id = db.Column(db.String(400), nullable=False)  # the id of the buyer favorite (seller or product)
-    favorite_type = db.Column(db.String(400), nullable=False)  # seller or product
-
-    # Define relationships (many-many)
-    sellers = db.relationship('Seller', secondary='favorites_seller_product', back_populates='favorites')
-    products = db.relationship('Product', secondary='favorites_seller_product', back_populates='favorites')
+    favorite_id = db.Column(db.String(400), nullable=False, unique=True)
+    buyer_id = db.Column(db.Integer, db.ForeignKey('buyers.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
 
     # Define buyer relationship
-    buyer_id = db.Column(db.Integer, db.ForeignKey('buyers.id'), nullable=False)
-    buyer = db.relationship("Buyer", back_populates="favorites")
-
-    """
-    def __init__(self,buyer_id,product_id,id):
-        if id is not None:
-            self = self.get_favorite_using_id(id)
-        else:
-            self.buyer_id = buyer_id
-            self.product_id = product_id
-            
-    """
-
-    @classmethod
-    def get_favorite_using_id(cls, _id):
-        return cls.query.filter_by(id=_id).first()
-
-    @classmethod
-    def delete_all_buyer_favorites(cls, buyer_id):
-        return cls.filter_by(buyer_id=buyer_id).delete()
-
-    @classmethod
-    def get_all_buyer_favorites(cls, buyer_id):
-        return cls.query.filter_by(buyer_id=buyer_id).all()
-
-    @classmethod
-    def get_all_buyer_favorites_that_are_sellers(cls, buyer_id):
-        return cls.query.filter_by(buyer_id=buyer_id).filter(favorite_type="seller").all()
-
-    @classmethod
-    def get_all_buyer_favorites_that_are_products(cls, buyer_id):
-        return cls.query.filter_by(buyer_id=buyer_id, favorite_type="product").all()
+    buyer = db.relationship('Buyer', back_populates='favorites')
+    # Define product relationship
+    product = db.relationship('Product', back_populates='favorites')
 
     @staticmethod
     def generate_unique_id():
-        unique_id = str(uuid.uuid4()).encode()
-        return hashlib.sha256(unique_id).hexdigest()
+        return str(uuid.uuid4())
+
+    @classmethod
+    def get_favorite_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def get_favorites_by_buyer_id(cls, buyer_id):
+        return cls.query.filter_by(buyer_id=buyer_id).all()
 
     def save_to_db(self):
+        if not self.favorite_id:
+            self.favorite_id = self.generate_unique_id()
         db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
 
     def delete_from_db(self):
         db.session.delete(self)
-        db.session.commit()
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
