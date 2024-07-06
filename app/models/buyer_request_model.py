de85fd2ec1c6982f0a6c48cef067ea23d3fe0046
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-07-06 10:15:23 +0100

    Improvements: redundant imports, timestamp field, category query, error handling

--- app/models/buyer_request_model.py
+++ app/models/buyer_request_model.py
@@ -1,6 +1,5 @@
 from db import db
-import time
-import hashlib
+from sqlalchemy.sql import func
 import uuid
 
 
@@ -8,11 +7,10 @@ class BuyerRequest(db.Model):
     __tablename__ = "buyer_request"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    unique_id = db.Column(db.String(400), nullable=False)
-    # buyer_id = db.Column(db.String(400), db.ForeignKey('buyers.unique_id'), nullable=False)
+    unique_id = db.Column(db.String(400), nullable=False, unique=True)
     product_description = db.Column(db.String(400), nullable=False)
     category = db.Column(db.String(255))
-    created_at = db.Column(db.TIMESTAMP, default=time.time(), nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now(), nullable=False)
     status = db.Column(db.String(255), default="open")
 
     # Define buyer relationship
@@ -22,21 +20,9 @@ class BuyerRequest(db.Model):
     # Define seller relationship (many-many)
     sellers = db.relationship('Seller', secondary='seller_buyer_query', back_populates='buyer_requests')
 
-    """
-    def __init__(self, buyer_id, product_description, category, unique_id):
-        if unique_id is not None:
-            self = self.get_requests_through_id(unique_id)
-        else:
-            self.unique_id = self.generate_unique_id()
-            self.buyer_id = buyer_id
-            self.product_description = product_description
-            self.category = category
-    """
-
     @staticmethod
     def generate_unique_id():
-        unique_id = str(uuid.uuid4()).encode()
-        return hashlib.sha256(unique_id).hexdigest()
+        return str(uuid.uuid4())
 
     @classmethod
     def get_requests_through_id(cls, _id):
@@ -48,17 +34,26 @@ class BuyerRequest(db.Model):
 
     @classmethod
     def get_requests_using_category(cls, *args):
-        return cls.query.filter_by(cls._in(list(args))).all()
+        return cls.query.filter(cls.category.in_(args)).all()
 
     @classmethod
     def delete_all_buyer_requests(cls, buyer_id):
         return cls.query.filter_by(buyer_id=buyer_id).delete()
 
     def save_to_db(self):
-        self.unique_id = self.generate_unique_id()
+        if not self.unique_id:
+            self.unique_id = self.generate_unique_id()
         db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
 
     def delete_from_db(self):
         db.session.delete(self)
-        db.session.commit()
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
