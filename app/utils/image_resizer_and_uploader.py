0653b7941994b4b04676ce267fde6c1b4c7b3e3d
Joshua <amethystguitaristj@gmail.com>
2024-11-19 09:04:18 +0000

    fixes for the image resizer

--- app/utils/image_resizer_and_uploader.py
+++ app/utils/image_resizer_and_uploader.py
@@ -3,9 +3,10 @@ import uuid
 from rembg import remove
 import os
 
+Image.MAX_IMAGE_PIXELS = 933120000
 
 def generate_image_name():
-    return str(uuid.uuid5)
+    return str(uuid.uuid4())
 
 
 class ImageSaver:
@@ -38,33 +39,37 @@ class ImageSaver:
                 width, height = image.size
                 new_width = width * 0.3
                 new_height = height * 0.3
-                image.resize((new_width, new_height))
+                image.resize((int(new_width), int(new_height)))
                 image_name = generate_image_name()
-                image.save("app/uploads/" + image_name + ".jpg", "JPEG", optimize=True, quality=50)
+                image.save("../uploads/" + image_name + ".jpg", "JPEG", optimize=True, quality=50)
                 return image_name
-        except OSError:
+        except OSError as e:
+            print(str(e))
             return False
 
-    @classmethod
-    def compress_image_and_remove_background(cls, image_location):
-        """compress an image
-        
+    """compress an image and removes its background
+
         Keyword arguments:
         image_location - the directory where the image is located
         Return: a boolean
-        """
+    """
+    @classmethod
+    def compress_image_and_remove_background(cls, image_location):
+        
         try:
             with Image.open(image_location) as image:
                 width, height = image.size
                 new_width = width * 0.3
                 new_height = height * 0.3
-                image.resize((new_width, new_height))
+                image.resize((int(new_width), int(new_height)))
                 image_name = generate_image_name()
+                if image.mode in ("RGBA", "P"): image = image.convert('RGB')
                 image_with_background_removed = remove(image)
-                image_with_background_removed.save("app/uploads/" + image_name + ".jpg", "JPEG", optimize=True,
+                image_with_background_removed.save("../uploads/" + image_name + ".png", "PNG", optimize=True,
                                                    quality=50)
                 return image_name
-        except OSError:
+        except OSError as e:
+            print(str(e))
             return False
 
     @classmethod
@@ -86,3 +91,6 @@ class ImageSaver:
             return True
         except OSError:
             return False
+
+image_saver = ImageSaver()
+print(image_saver.compress_image_and_remove_background("C:/Users/Administrator/markt_python/app/utils/samsung-memory-jbmjneY3a6g-unsplash.jpg"))
